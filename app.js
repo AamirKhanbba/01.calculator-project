096fbad63f360c5132ca45a26ee55b466bf257b6
AamirKhanbba <163715033+AamirKhanbba@users.noreply.github.com>
2024-03-27 12:54:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberOne",
+        message: "Enter your first number:"
+    },
+    {
+        type: "list",
+        name: "operator",
+        message: "Enter your operator:",
+        choices: ["+", "-", "*", "/"]
+    },
+    {
+        type: "number",
+        name: "numberTwo",
+        message: "Enter your second number:"
+    }
+]);
+// Destructuring assignment of answers
+const { numberOne, operator, numberTwo } = answers;
+// Perform the arithmetic operation
+let result;
+switch (operator) {
+    case "+":
+        result = numberOne + numberTwo;
+        break;
+    case "-":
+        result = numberOne - numberTwo;
+        break;
+    case "*":
+        result = numberOne * numberTwo;
+        break;
+    case "/":
+        result = numberOne / numberTwo;
+        break;
+    default:
+        console.log("Invalid operator");
+}
+// Output the operation expression and result
+console.log(`${numberOne} ${operator} ${numberTwo} = ${result}`);
